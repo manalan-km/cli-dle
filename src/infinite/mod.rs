53b4ec4c7a4a2297e0782b823833abbff67c3b77
manalankm <>
2025-05-26 21:45:04 +1000

    chore: reduced build time

--- src/infinite/mod.rs
+++ src/infinite/mod.rs
@@ -1,15 +1,15 @@
-use crate::{data::{self, get_wordles}, run_game};
+use crate::{data::{ get_wordles}, run_game};
 use rand::prelude::*;
 use std::io;
 
-fn set_wordle () -> &'static String {
+fn set_wordle () -> String {
 
-    let wordle = data::WORDLES.get_or_init(get_wordles);
+    let wordles = get_wordles();
     let mut rng = rand::rng();
-    let index = rng.random_range(0..wordle.len() + 1);
-    &wordle[index]
+    let index = rng.random_range(0..wordles.len() + 1);
+    let wordle = wordles[index].clone();
 
-
+    wordle
 }
 
 
@@ -17,7 +17,7 @@ fn set_wordle () -> &'static String {
 pub fn run() {
     println!("Running infinite mode");
 
-    loop{
+   loop{
         let wordle = set_wordle().to_string();
         run_game(&wordle);
         println!("Wordle for this round: {wordle}");
