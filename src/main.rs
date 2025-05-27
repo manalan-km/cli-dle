79dfc3ef8081b4a37d699fbb3aef7707089d9c0a
Manalan <109595890+manalan-km@users.noreply.github.com>
2025-05-27 11:06:10 +1000

    Revert "feat: release mechanism"

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use cli_dle;
+use wordle_cli;
 use std::{env, process};
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let game_mode = cli_dle::Config::build_config(args).unwrap_or_else(|err|{
+    let game_mode = wordle_cli::Config::build_config(args).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
-    cli_dle::run(game_mode.mode);    
+    wordle_cli::run(game_mode.mode);    
     
 }
